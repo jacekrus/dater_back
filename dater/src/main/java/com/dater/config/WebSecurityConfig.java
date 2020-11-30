0510a93631ac5e90ad47c1cff471b3ffabd14c82
jacekrus <jacek1807@gmail.com>
2020-11-30 23:26:05 +0100

    CSRF protection has been enabled
    -WebSecurityConfig now work with CSRF cookie set on the client
    -CORS config had to be properly adjusted to work together with CSRF

--- dater/src/main/java/com/dater/config/WebSecurityConfig.java
+++ dater/src/main/java/com/dater/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.dater.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,17 +12,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.dater.service.UserService;
 
 @Configuration
-@EnableWebSecurity(debug = true)
-public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	private static final String LOGIN_URL = "/datrLogin";
 	private static final String LOGOUT_URL = "/datrLogout";
@@ -36,14 +41,12 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.DELETE, "/users").hasAuthority("ADMIN")
-			.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.antMatchers(HttpMethod.POST, "/users/add").permitAll()
 			.antMatchers(HttpMethod.POST, "/users/validate").permitAll()
 			.antMatchers(HttpMethod.POST, "/passwords", "/passwords/reset").permitAll()
 			.antMatchers(HttpMethod.GET, "/passwords").permitAll()
 			.anyRequest().authenticated()
 			.and()
-			.addFilterBefore(new CorsFilterConfig(), ChannelProcessingFilter.class)
 			.addFilterBefore(authFilter(), UsernamePasswordAuthenticationFilter.class)
 			.exceptionHandling()
 			.accessDeniedHandler((request, response, accessDeniedException) -> response.setStatus(HttpStatus.FORBIDDEN.value()))
@@ -56,7 +59,8 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 			.invalidateHttpSession(true)
 			.logoutUrl(LOGOUT_URL)
 			.logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
-			.and().csrf().disable();
+			.and().cors()
+			.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 	}
 	
 	@Override
@@ -72,6 +76,18 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 		filter.setAuthenticationManager(super.authenticationManager());
 		filter.setFilterProcessesUrl(LOGIN_URL);
 		return filter;
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "PATCH", "DELETE"));
+		configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization", "Content-Length", "Accept", "X-Requested-With", "X-XSRF-TOKEN"));
+		configuration.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 }
